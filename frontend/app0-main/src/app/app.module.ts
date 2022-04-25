6ec0d4cd1489e57a38fc8148ec7b58dbd2acb3ce
王斌 <253498229@qq.com>
2022-04-25 17:30:45 +0800

    添加 ng-zorro-antd

--- frontend/app0-main/src/app/app.module.ts
+++ frontend/app0-main/src/app/app.module.ts
@@ -4,6 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { zh_CN } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
@@ -13,8 +22,11 @@ import { EmptyRouteComponent } from './empty-route/empty-route.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent],
 })
 export class AppModule {
